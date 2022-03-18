4e8ad947d6fdc621a588cb92823de6f438bfbc31
SatoriRae <satorii@ymail.com>
2022-03-17 20:39:34 -0600

    getting started

--- app.js
+++ app.js
@@ -2,35 +2,73 @@
  * Stores the list of kittens
  * @type {Kitten[]}
  */
+
 let kittens = [];
 /**
  * Called when submitting the new Kitten Form
  * This method will pull data from the form
  * use the provided function to give the data an id
  * you can use robohash for images
- * https://robohash.org/<INSERTCATNAMEHERE>?set=set4
+ * https://robohash.org/<$(kittenName)>?set=set4
  * then add that data to the kittens list.
  * Then reset the form
  */
-function addKitten(event) {}
+function addKitten(event) {
+  event.preventDefault()
+  let form = event.target
+}
 
+let kitten = {
+  id: generateId(),
+  name: form.name.value,
+  mood: "",
+  affection: "",
+}
+
+kittens.push(kitten)
+saveKittens()
+form.reset()
 /**
  * Converts the kittens array to a JSON string then
  * Saves the string to localstorage at the key kittens
  */
-function saveKittens() {}
-
+function saveKittens() {
+  window.localStorage.setItem("kittens", JSON.stringify(kittens))
+  console.log ("saved kitten-list to local storage")
+  drawKittens();
+}
 /**
  * Attempts to retrieve the kittens string from localstorage
  * then parses the JSON string into an array. Finally sets
  * the kittens array to the retrieved array
  */
-function loadKittens() {}
+function loadKittens() {
+ let storedKittens = JSON.parse(window.localStorage.getItem("contacts"))
+ if (storedKittens){
+   kittens = storedKittens
+   console.log ("brought kitten-list storage up at page refresh") 
+ }
+}
 
 /**
  * Draw all of the kittens to the kittens element
  */
-function drawKittens() {}
+function drawKittens() {
+let kittenListElement = document.getElementById("kitten-list")
+let kittenTemplate = ""
+kittens.forEach(kitten => {
+  <div id="kittens" class="d-flex align-items-center flex-wrap">
+ $(kitten.id)
+ $(kitten.name)
+ $(kitten.url)
+ $(kitten.mood)
+ $(kitten.affection)
+
+})
+
+kittenListElement.innerHTML = kittenTemplate{
+  console.log("template card added/deleted")
+}
 
 /**
  * Find the kitten in the array by its id
